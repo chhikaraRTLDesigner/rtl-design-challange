28005615119af8522dab8acbd2ba0b59f4652b5e
chhikaraRTLDesigner <anujkumarc24@gmail.com>
2026-06-11 17:44:06 +0530

    Initial Code added

--- 001_UP_Counter/RTL/up_counter.v
+++ 001_UP_Counter/RTL/up_counter.v
@@ -1,3 +1,32 @@
-module up_counter;
-
+module up_counter #(
+  parameter COUNT_WIDTH = 16)
+  (
+    input clk ,
+    input reset_n ,
+    input enable ,
+    input [COUNT_WIDTH - 1 : 0] count_value ,
+    output reg count_done,
+    output reg [COUNT_WIDTH - 1 : 0] count;
+  );
+  
+  always @ (posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+          count <= 'h0;
+          count_done <= 1'b0;
+    end
+    else begin
+      count_done <= 1'b0;
+     if (enable) begin
+      if (count == count_value) begin
+          count <= 'h0;
+          count_done <= 1'b1;
+      end
+      else begin 
+          count <= count + 1;
+          count_done <= 1'b0;
+      end 
+     end
+    end
+  end
+            
 endmodule
